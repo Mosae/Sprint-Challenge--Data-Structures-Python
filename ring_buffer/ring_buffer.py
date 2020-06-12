f3df78c0c6e84d0201433267d46f85b0d901b40f
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-06-12 11:43:10 -0500

    RingBuffer passes test

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,26 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = [None] * capacity  # place to store the data concat int
+        self.current = 0  # len is a tracker
+
+        # ds cannot be larger than capacity
 
     def append(self, item):
-        pass
+        # update len
+        if len(self.storage) < self.capacity:  # storage has to be < capacity
+            self.storage.append(item)  # add item to storage
+        else:
+            self.storage[self.current] = item  # will move to next index
+        # if storage is > than capacity - start at 0(current)
+        if self.current < len(self.storage) - 1:
+            self.current += 1
+        else:
+            self.current = 0
 
     def get(self):
-        pass
+        new_items = []  # create a new list
+        for i in self.storage:  # loop through the storage
+            if i is not None:
+                new_items.append(i)
+        return new_items
