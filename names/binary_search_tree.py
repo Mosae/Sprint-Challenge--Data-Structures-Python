117a71cbcda30447dd0b728727a66f8419ff402b
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-06-12 12:21:59 -0500

    names complete

---
+++ names/binary_search_tree.py
@@ -0,0 +1,109 @@
+"""
+Binary search trees are a data structure that enforce an ordering over
+the data they store. That ordering in turn makes it a lot more efficient
+at searching for a particular piece of data in the tree.
+
+This part of the project comprises two days:
+1. Implement the methods `insert`, `contains`, `get_max`, and `for_each`
+   on the BSTNode class.
+2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
+   on the BSTNode class.
+"""
+from sys import maxsize
+from collections import deque
+maxNum = maxsize
+
+
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value < self.value:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = BSTNode(value)
+        else:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BSTNode(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        # base case
+        if self.value == target:
+            return True
+            # which direction to go
+        if self.value > target:
+            # go left
+            if not self.left:
+                return False
+
+            else:
+                return self.left.contains(target)
+
+        else:
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
+
+    # Return the maximum value found in the tree
+
+    def get_max(self):
+        # right till you cant go right anymore
+        if not self.right:
+            return self.value
+        return self.right.get_max()
+
+    # Call the function `fn` on the value of each node
+    # Note that this does not return anything
+    def for_each(self, fn):
+        # This should traverse every tree node
+        fn(self.value)
+
+        if self.left:
+            self.left.for_each(fn)
+
+        if self.right:
+            self.right.for_each(fn)
+
+    # Part 2 -----------------------
+
+    # Print all the values in order from low to high
+    # Hint:  Use a recursive, depth first traversal
+    def in_order_print(self, node):
+        # keep order of nodes visited
+        if node.left:
+            self.in_order_print(node.left)
+        if node.right:
+            self.in_order_print(node.right)
+            print(node.value)
+            print(node.value)
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative breadth first traversal
+    def bft_print(self, node):
+        pass
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative depth first traversal
+    def dft_print(self, node):
+        pass
+
+    # Stretch Goals -------------------------
+    # Note: Research may be required
+
+    # Print Pre-order recursive DFT
+    def pre_order_dft(self, node):
+        pass
+
+    # Print Post-order recursive DFT
+    def post_order_dft(self, node):
+        pass
